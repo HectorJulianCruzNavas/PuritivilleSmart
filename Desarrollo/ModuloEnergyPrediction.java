90421412ee8dfbd39fe49c739a2f9bd6df79fc4e
HectorJulianCruzNavas <cruznavas2208@gmail.com>
2024-10-27 16:45:20 -0500

    Add files via upload
    CODIGO PRINCIPAL

---
+++ Desarrollo/ModuloEnergyPrediction.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sensoriot;
+
+/**
+ *
+ * @author Julian2208
+ */
+public class ModuloEnergyPrediction {
+public void predecir(String datosEnergia) {
+        System.out.println("Predicción de demanda energética: " + datosEnergia);
+    }
+}    
+
---
+++ Desarrollo/ModuloWaterPrediction.java
@@ -0,0 +1,13 @@
+
+package sensoriot;
+
+/**
+ *
+ * @author Julian2208
+ */
+public class ModuloWaterPrediction {
+  public void predecir(String datosAgua) {
+        System.out.println("Predicción de uso de agua: " + datosAgua);
+    }
+}  
+
